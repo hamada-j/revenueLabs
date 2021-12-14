3008ff51e8934b13ada6f07d60a3b5ed55cfcf0e
Hamada <defjamvk@gmail.com>
2021-12-15 00:34:20 +0100

    done
    done

--- revenuelabs_back/controller/getAllCivilizations.js
+++ revenuelabs_back/controller/getAllCivilizations.js
@@ -1,34 +1,3 @@
-// 'use strict';
-// const externalAPI = require("../services/ExternalApi/getExternalData");
-// const { API_CIVILIZATIONS, API_CIVILIZATION } = process.env;
-
-// exports.getData = async (req, res, next) => {
-//   try {
-//     await externalAPI.getData(API_CIVILIZATIONS).then(async (response) => {
-//       const data = response.civilizations;
-//       res.status(200).json({ civilizations: data});         
-//     }).catch((error) => {
-//       res.status(401).json({ error: `error external API\n ${error}` });  
-//     });  
-//   } catch (error) {
-//     res.status(502).json({ message: `Could not get data from error external API!`, error: error})
-//   }
-// }
-
-// exports.getDataById = async (req, res, next) => {
-//   const id = req.params.id;
-//   try {
-//     await externalAPI.getData(`${API_CIVILIZATION}/${id}`).then(async (response) => {
-//       const data = [response];   
-//       res.status(200).json({ civilization: data});         
-//     }).catch((error) => {
-//       res.status(401).json({ error: `error external API\n ${error}` });  
-//     });   
-//   } catch (error) {
-//     res.status(502).json({ message: `Could not get data from error external API!`, error: error})
-//   }
-// }
-
 'use strict';
 const externalAPI = require("../services/ExternalApi/getExternalData");
 const { API_CIVILIZATIONS, API_CIVILIZATION } = process.env;
--- revenuelabs_back/controller/getMoreDataForCivilization.js
+++ revenuelabs_back/controller/getMoreDataForCivilization.js
@@ -1,21 +1,3 @@
-// 'use strict';
-// const externalAPI = require("../services/ExternalApi/getExternalData");
-
-// exports.getData = async (req, res, next) => {
-//   const url = req.body.url;
-//   try {
-//     await externalAPI.getData(url).then(async (response) => {
-//       const data = [];
-//       data.push(response);
-//       res.status(200).json({ extraData: data });        
-//     }).catch((error) => {
-//       res.status(401).json({ error: 'error external API \n' + error });  
-//     });
-//   } catch (error) { 
-//     res.status(502).json({ message: `Could not get data from error external API!`, error: error})
-//   }
-// }
-
 'use strict';
 const externalAPI = require("../services/ExternalApi/getExternalData");
 
